507253930997ca81e57ac692c5c85ddda582ec1d
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-08-10 19:48:01 +0500

    komiks_database

---
+++ src/database/komiks_db.js
@@ -0,0 +1,14 @@
+import {bob_1} from './pdf_filelar/Jabrmaqom/Jabrmaqom_1_bob.pdf'
+
+export const komiks_data = [
+    {
+        komiks_name:'Jabrmaqom',
+        holati:'Davom etyamoqda...',
+        janri:'tarixiy, action, romantika',
+        muallif:'Shaxrizoda_art',
+        davlat:'Uzbekistan',
+        yili:'2013-yildan chiqa boshlagan',
+        boblar:'9+',
+        boblar:[bob_1]
+    }
+]
