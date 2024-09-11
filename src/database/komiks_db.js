22c35d0666fbf952698b6af540c710bdc1b4696a
Davron Chetin <nabijonovdavronbek619@gmail.com>
2024-09-11 08:47:19 +0500

    burushli_studio

--- src/database/komiks_db.js
+++ src/database/komiks_db.js
@@ -1,3 +1,4 @@
+// Jabrmaqom pdf files
 import bob_1 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_1_Bob.pdf";
 import bob_2 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_2_Bob.pdf";
 import bob_3 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_3_Bob.pdf";
@@ -7,14 +8,20 @@ import bob_6 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_6_Bob.pdf";
 import bob_7 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_7_Bob.pdf";
 import bob_8 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_8_Bob.pdf";
 import bob_9 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_9_Bob.pdf";
+import bob_10 from "./pdf_filelar/Jabrmaqom/Jabrmaqom_10_bob.pdf";
+
+// o`lmayman pdf files
+
+
 // images
 import zoda from "./pdf_filelar/Jabrmaqom/zoda_img_logo.jpg";
-import komik_img from "./pdf_filelar/Jabrmaqom/Jabrmaqom_img.jpg";
+import jabrmaqom_komik_img from "./pdf_filelar/Jabrmaqom/Jabrmaqom_img.jpg";
+
 
 export const komiks_data = [
   {
     komiks_name: "Jabrmaqom",
-    komiks_img: komik_img,
+    komiks_img: jabrmaqom_komik_img,
     holati: "Davom etmoqda...",
     janri: "tarixiy, action, romantika",
     muallif: "Shaxrizoda_art",
@@ -22,6 +29,6 @@ export const komiks_data = [
     davlat: "Uzbekistan",
     yili: "2013-yildan chiqa boshlagan",
     boblar: "9+",
-    boblar_pdf: [bob_1, bob_2, bob_3, bob_4, bob_5, bob_6, bob_7, bob_8, bob_9],
+    boblar_pdf: [bob_1, bob_2, bob_3, bob_4, bob_5, bob_6, bob_7, bob_8, bob_9,bob_10],
   },
 ];
--- src/modal/pdf_modal.js
+++ src/modal/pdf_modal.js
@@ -1,30 +1,65 @@
 import React, { useState } from "react";
 import "./pdf_modal.scss";
+import { Document, Page } from 'react-pdf';
+// import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 export default function Pdf_modal({ groups, closeTogle }) {
   const [indexMovie, setIndexMovie] = useState(groups[0]);
+  // const [file_pdf, setFile_pdf] = useState(groups[1]); 
   return (
-    <div className="pdf_modal">
-      <div className="modal_body">
-        <button
-          onClick={() => {
-            closeTogle();
-          }}
-          className="close_btn"
-        >
-          Ortga
-        </button>
+    <div className="for_fix">
+      <div className="pdf_modal">
+        <div className="modal_body">
+          <button
+            onClick={() => {
+              closeTogle();
+            }}
+            className="close_btn"
+          >
+            Ortga
+          </button>
 
-        <div className="body">
-          <iframe src={indexMovie} width="100%" height="600px"></iframe>
-        </div>
+          <div className="body">
+            <iframe src={indexMovie} width="100%" height="600px"></iframe>
+            {/* <PdfReader file={file_pdf}/> */}
+          </div>
 
-        <div className="parts">
-          {groups.map((r, index) => (
-            <button key={index}>{index + 1} - bob</button>
-          ))}
+          <div className="parts">
+            {groups.map((r, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  setIndexMovie(groups[index]);
+                }}
+              >
+                {index + 1} - bob
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+// function PdfReader({file}) {
+//   const [numPages, setNumPages] = useState(null);
+  
+
+//   // Called when the PDF is loaded
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
+//   return (
+//     <>
+//       <div className="pdf-reader-container">
+//         <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+//           {Array.from(new Array(numPages), (el, index) => (
+//             <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+//           ))}
+//         </Document>
+//       </div>
+//     </>
+//   );
+// }
